0936cbe226ded3c253e774e1031731ad155d7cfd
justacurley <curleylax@gmail.com>
2018-07-09 08:03:02 -0600

    preliminary function

--- Set-Q5API.ps1
+++ Set-Q5API.ps1
@@ -1,19 +1,18 @@
-
-
-$foo = @{ 
-        URI     = 'https://q.daskeyboard.com/api/1.0/users/authorized_clients' 
-        Headers = @{'Authorization' = 'Bearer {0}' -f $oauth.access_token}
-} 
-
-
-Invoke-RestMethod -URI $foo.uri -headers $foo.headers -ContentType application/json
-# $bar = @{ 
-#     URI     = 'https://q.daskeyboard.com/oauth/1.4/token' 
-#     Headers = @{'accept' = 'application/json'}
-#     Method  = 'Post'
-#     Body    = @{
-#         'client_id'     = $clientID
-#         'client_secret' = $ClientSC
-#         'grant_type'    = "client_credentials"
-#     }
-# }
+function Set-Q5API {
+        [CmdletBinding()]
+        param (
+            #Full oAuth object returned by Get-Q5oAuth.ps1
+            [Parameter(Mandatory = $true,
+                ValueFromPipelineByPropertyName = $true)]
+            [validatenotnullorempty()]
+            $oAuth,
+          
+            #API function to call
+            [Parameter(Mandatory = $true,
+                ValueFromPipelineByPropertyName = $true)]
+            [validateSet('RevokeClient', 'NewSignal', 'UpdateSignal', 'DeleteSignal', 'Zones', 'Effects', 'GetSignal')]
+            [alias('API')]
+            [string]
+            $APIFunction
+        )
+}
